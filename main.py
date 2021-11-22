6230f934e6de11477e5406f951e565226f44f50c
leniks <anciborko04@mail.ru>
2021-11-22 22:43:31 +0300

    scnd

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import sys
 import random
 
+from UI import Ui_MainWindow
 from PyQt5 import uic
 from PyQt5.QtCore import QPoint
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 from PyQt5.QtGui import QPainter, QBrush, QColor
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        # uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.paint)
         print(self.width())
 
@@ -20,7 +22,7 @@ class MyWidget(QMainWindow):
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
 
         point = QPoint(random.randint(0, self.width()), random.randint(0, self.height()))
         a = random.randint(0, self.width() // 2)
